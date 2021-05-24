41e8954c2c7c2cc8ed69b1d6b52774ac892d7bc9
Logvina <irinlogvina9@gmail.com>
2021-05-25 00:20:29 +0300

    Добавила разметку

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-import galleryItems from './gallery-items';
+import images from './gallery-items.js';
+
+const refs = {
+  gallery: document.querySelector('.js-gallery'),
+
+
+}
+
+const createGalleryItemsMurkup = function (images) {
+  return images
+  .map(
+    ({preview, original, description}) => 
+      `<li class="gallery__item">
+<a
+class="gallery__link"
+href="${original}"
+>
+<img
+  class="gallery__image"
+  src="${preview}"
+  data-source="${original}"
+  alt="${description}"
+/>
+</a>
+</li>`,
+  ).join('');
+};
+
+const galleryMurkup = createGalleryItemsMurkup(images)
+refs.gallery.insertAdjacentHTML('beforeend', galleryMurkup);
