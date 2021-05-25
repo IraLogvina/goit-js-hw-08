2e5c613358eda229bf2407cbfc02a3a8df06adf9
Logvina <irinlogvina9@gmail.com>
2021-05-25 23:25:38 +0300

    Обязательные задания

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@ import images from './gallery-items.js';
 
 const refs = {
   gallery: document.querySelector('.js-gallery'),
-
+  closeModalBtn: document.querySelector('[data-action="close-lightbox"]'),
+  modal: document.querySelector('.js-lightbox'),
+  modalImg: document.querySelector('.lightbox__image'),
 
 }
 
@@ -29,4 +31,29 @@ href="${original}"
 };
 
 const galleryMurkup = createGalleryItemsMurkup(images)
-refs.gallery.insertAdjacentHTML('beforeend', galleryMurkup);
+refs.gallery.insertAdjacentHTML('beforeend', galleryMurkup),
+
+// Открытие и закрытие модального окна
+
+refs.gallery.addEventListener('click', openModalImg);
+refs.closeModalBtn.addEventListener('click', closeModalImg);
+
+function openModalImg (evt) {
+  evt.preventDefault();
+  if (evt.target.localName !== 'img') {
+    return;
+  }
+
+  refs.modal.classList.add('is-open');
+
+  refs.modalImg.src = evt.target.dataset.source;
+  refs.modalImg.alt = evt.target.alt;
+
+}
+
+function closeModalImg() {
+  refs.modal.classList.remove('is-open');
+  refs.modalImg.src = '';
+  refs.modalImg.alt = '';
+}
+
